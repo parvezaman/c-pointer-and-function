3820066263d9057d83224a27aeba40ebffe140f7
parvezaman <emailtoamanullah@gmail.com>
2022-06-16 08:46:52 +0600

    getSum function

---
+++ getSum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int getSum(int *arr, int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int sum = getSum(arr, n);
+    printf("The sum is %d\n", sum);
+    return 0;
+}
